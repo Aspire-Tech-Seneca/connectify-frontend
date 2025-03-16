e4a94722adc4dee6e47f61bd0321eb92cb9b2b65
zahrah-glitch <zahraharif40@gmail.com>
2025-03-16 01:46:24 -0400

    excluded styling in signup.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import Profile from "./components/profile";
 import HomePage from "./components/HomePage"; 
 import AboutPage from "./components/AboutPage";
-import peachImage from "./peach.jpg"; // Background image
+import peachImage from "./peach.jpg"; // Background image for all pages except signup
 import Signup from "./components/Signup";
 import WelcomePage from "./components/WelcomePage";
 import ChatPage from "./components/ChatPage";
@@ -17,41 +17,49 @@ import ViewEvents from "./components/ViewEvents";
 import PolicyCompliance from "./components/PolicyCompliance";
 import NotificationPage from "./components/NotificationPage";
 
+const AppContent = () => {
+  const location = useLocation(); // Get current route
 
-const App = () => {
-  const appStyles = {
-    backgroundImage: `url(${peachImage})`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
-  };
+  // Apply background only if it's NOT the signup page
+  const appStyles = location.pathname !== "/signup"
+    ? {
+        backgroundImage: `url(${peachImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }
+    : {}; // No background styling for signup
 
   return (
-    <Router>
-      <div className="App" style={appStyles}>
-        <Routes>
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/Home" element={<HomePage />} /> 
-          <Route path="/notifications" element={<NotificationPage />} />
-          <Route path="/about" element={<AboutPage />} /> 
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/matches" element={<MyMatches/>} />
-          <Route path="/ChatPage" element={<ChatPage/>} />
-		  <Route path="/UserSettings" element={<UserSettings />} />
-		  <Route path="/ViewEvents" element={<ViewEvents />} />
-		  <Route path="/createevent" element={<CreateEvent />} />
-      <Route path="/CommunityChat" element={<CommunityChat />} />  
-      <Route path="/PolicyCompliance" element={<PolicyCompliance />} /> 
-        </Routes>
-      </div>
-    </Router>
+    <div className="App" style={appStyles}>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/home" element={<HomePage />} /> 
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/about" element={<AboutPage />} /> 
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/matches" element={<MyMatches />} />
+        <Route path="/ChatPage" element={<ChatPage />} />
+        <Route path="/UserSettings" element={<UserSettings />} />
+        <Route path="/ViewEvents" element={<ViewEvents />} />
+        <Route path="/createevent" element={<CreateEvent />} />
+        <Route path="/CommunityChat" element={<CommunityChat />} />
+        <Route path="/PolicyCompliance" element={<PolicyCompliance />} />
+      </Routes>
+    </div>
   );
 };
+
+const App = () => (
+  <Router>
+    <AppContent />
+  </Router>
+);
 
 export default App;
