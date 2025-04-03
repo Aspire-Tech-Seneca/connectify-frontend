80cbb969707a71652093f6007d4db0e913744da4
EniZeqo28 <enizeqo28@gmail.com>
2025-04-03 19:55:26 -0400

    Added Navbar routes and what should be excluded

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
@@ -5,61 +6,70 @@ import LoginForm from "./components/LoginForm";
 import Profile from "./components/profile";
 import HomePage from "./components/HomePage"; 
 import AboutPage from "./components/AboutPage";
-import peachImage from "./peach.jpg"; // Background image for all pages except signup
-import abstract from "./abstract.jpg";
 import Signup from "./components/Signup";
 import WelcomePage from "./components/WelcomePage";
 import ChatPage from "./components/ChatPage";
 import CreateEvent from "./components/CreateEvent";
 import MyMatches from "./components/MyMatches";
 import UserSettings from "./components/UserSettings";
-import CommunityChat from "./components/CommunityChat.js";
+import CommunityChat from "./components/CommunityChat";
 import ViewEvents from "./components/ViewEvents";
 import PolicyCompliance from "./components/PolicyCompliance";
 import NotificationPage from "./components/NotificationPage";
+import UserProfile from "./components/userProfile";
+import CustomNavbar from "./components/CustomNavbar";
+import abstract from "./abstract.jpg";
 
 const AppContent = () => {
-  const location = useLocation(); // Get current route
+  const location = useLocation();
 
-  // Apply background only if it's NOT the signup page
+  // Define routes where the navbar should NOT be shown.
+  const noNavbarRoutes = ["/login", "/Signup", "/", "/welcome"];
+  const showNavbar = !noNavbarRoutes.includes(location.pathname);
+
+  // Background styling (applied to all pages except login/Signup)
   const appStyles =
-  location.pathname !== "/Signup" && location.pathname !== "/login"
-    ? {
-        background: `linear-gradient(
-          rgba(100, 126, 135, 0.4),
-          rgba(255, 255, 255, 0.3)
-        ), url(${abstract})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-        backgroundAttachment: "fixed",        
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }
-    : {};
+    location.pathname !== "/Signup" && location.pathname !== "/login"
+      ? {
+          background: `linear-gradient(
+            rgba(100, 126, 135, 0.4),
+            rgba(255, 255, 255, 0.3)
+          ), url(${abstract})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+          backgroundAttachment: "fixed",
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }
+      : {};
 
   return (
     <div className="App" style={appStyles}>
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/home" element={<HomePage />} /> 
-        <Route path="/notifications" element={<NotificationPage />} />
-        <Route path="/about" element={<AboutPage />} /> 
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/matches" element={<MyMatches />} />
-        <Route path="/ChatPage" element={<ChatPage />} />
-        <Route path="/UserSettings" element={<UserSettings />} />
-        <Route path="/ViewEvents" element={<ViewEvents />} />
-        <Route path="/createevent" element={<CreateEvent />} />
-        <Route path="/CommunityChat" element={<CommunityChat />} />
-        <Route path="/PolicyCompliance" element={<PolicyCompliance />} />
-
-      </Routes>
+      {showNavbar && <CustomNavbar />}
+      {/* Add a top margin only if navbar is shown */}
+      <div style={{ marginTop: showNavbar ? "300px" : "", width: "100%" }}>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/home" element={<HomePage />} /> 
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/about" element={<AboutPage />} /> 
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/matches" element={<MyMatches />} />
+          <Route path="/ChatPage" element={<ChatPage />} />
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/UserSettings" element={<UserSettings />} />
+          <Route path="/ViewEvents" element={<ViewEvents />} />
+          <Route path="/createevent" element={<CreateEvent />} />
+          <Route path="/CommunityChat" element={<CommunityChat />} />
+          <Route path="/PolicyCompliance" element={<PolicyCompliance />} />
+        </Routes>
+      </div>
     </div>
   );
 };
